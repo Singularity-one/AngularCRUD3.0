33df3060d81b6b56cf8c0c4843e21d7472a4cc75
Singularity-one <singularityqaz@gmail.com>
2019-12-19 16:09:25 +0800

    detailEnd

--- src/app/customer-details/customer-details.component.ts
+++ src/app/customer-details/customer-details.component.ts
@@ -28,63 +28,30 @@ export class CustomerDetailsComponent implements OnInit {
  protected scustomeridSQLid2: string[] = [];
 
   ngOnInit() {
-  this.scustomeridSQLid2 = this.customerService.messages;
-  console.log("有接到值"+this.scustomeridSQLid2 );
+    this.scustomeridSQLid2 = this.customerService.messages;
+    console.log("有接到值"+this.scustomeridSQLid2 );
+    this.customerId =this.scustomeridSQLid2.toString();
 
-  this.customerId =this.scustomeridSQLid2.toString();
-  this.findOne(this.customerId);
-  this.customerService.clear();//清除customerService中messages
+    this.customerService.findOne(this.customerId).subscribe(
+      res => {
+        console.log(res);
+          if(res['success']){
+          console.log("success");
+          }
 
-  }
-
-  findOne(customerId){
-    let customerIdStr = customerId;
-    let userJSON = {
-      'header': {
-        'msgId': '1',
-        'txnSeq': '2',
-        'branchId': '3',
-        'clientIp': '4'
-      },
-      'body': {
-        "customerId": customerId
+        const returnText = res['body'].returnCode;
+           if('0000'=== returnText){
+              console.log("成功拿到單筆回傳");
+              console.log(res);
+              const body = res['body'];
+              this.customers = body.dataList;
+              console.log(this.customers);
+            }
+      },errRes =>{
+      console.log(errRes);
       }
-    };
-
-       // 透過 JSON.parse() 解析 JSON 字串
-       let user = JSON.stringify(userJSON);
-       var newstr = user
-  
-       console.log(
-        "newstr"+newstr
-       );
-  
-       var objJsonArray =JSON.parse(newstr);
-
-     this.http.post('http://localhost:8080/customer/findSQL',objJsonArray
-    ,this.headers).subscribe(
-                 res => {
-                  console.log(res);
-                   if(res['success']){
-                     console.log("success");
-                   }
-                   const returnText = res['body'].returnCode;
-                   if('0000'=== returnText){
-                    console.log("成功拿到單筆回傳");
-                    console.log(res);
-                    const body = res['body'];
-                    this.customerId =body.customerId;
-                    this.name =body.name;
-                    this.addr =body.addr;
-                    this.age =body.age;
-                    this.tel =body.tel;
-                    this.customers = body.dataList;
-                    console.log(this.customers);
-                   }
-                 },errRes =>{
-                   console.log(errRes);
-                 }
-               );
+    );
+    this.customerService.clear();//清除customerService中messages
   }
 
   list(){
