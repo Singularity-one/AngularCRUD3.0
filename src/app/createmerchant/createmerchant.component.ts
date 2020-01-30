136fed3fde0bcfe4a66603242b90f690661948dc
Singularity-one <singularityqaz@gmail.com>
2020-01-30 09:23:43 +0800

    CreatemerchantPro1.1

--- src/app/createmerchant/createmerchant.component.ts
+++ src/app/createmerchant/createmerchant.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Merchant } from '../merchant';
+import { MerchantService } from '../merchant.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-createmerchant',
@@ -7,9 +10,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatemerchantComponent implements OnInit {
 
-  constructor() { }
+  merchants: Merchant[];
+  imgdata:string;
+
+  merchantId: string;
+  name: string;
+  addr: string;
+  tel: string;
+  pic: string;
+  submitted = false;
+
+  image;
+
+  newMerchant: Merchant=new Merchant();
+  updateMerchant: Merchant=new Merchant();
+
+  constructor(private merchantService: MerchantService,private router:Router) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(m: Merchant) {
+    console.log("Create Merchant");
+    console.log("Name"+this.newMerchant.name);
+    console.log("m:Name"+m.name);
+    m.pic=this.pic;
+    console.log("m:Pic"+m.pic);
+    this.submitted = true;
+
+    this.merchantService.createMerchant(m).subscribe(
+      res => {
+       console.log(res);
+       const returnText = res['body'].returnCode;
+        
+       if('0000'=== returnText){
+         console.log("新增成功");
+         const body = res['body'];
+         this.router.navigate(['merchant-find-all']); // <-- 導向merchant-find-all
+        }
+      },errRes =>{
+        console.log(errRes);
+      }
+    );
+
+  }
+
+  upload($event) : void {
+    console.log("路徑"+$event.target.value);
+    console.log(typeof($event.target.files[0]));//型別
+    this.readThis($event.target);
+   
+  }
+
+  readThis(inputValue: any): void {
+    var file:File = inputValue.files[0];
+    var myReader:FileReader = new FileReader();
+  
+    myReader.onloadend = (e) => {
+      this.image = myReader.result;//轉64編碼
+      this.pic=this.image.replace("data:image/png;base64,", "");//去除前面data:image/png;base64,
+    }
+    myReader.readAsDataURL(file);
   }
 
 }
--- src/app/merchant.service.ts
+++ src/app/merchant.service.ts
@@ -40,4 +40,26 @@ export class MerchantService {
     return this.http.get<Merchant[]>('http://localhost:8080/merchant/findAll');
   }
 
+  public createMerchant(m: Merchant){
+    console.log("createMerchant");
+    console.log("m:Name"+m.name);
+
+    this.json["body"]["name"]=m.name;//給值
+    this.json["body"]["addr"]=m.addr;
+    this.json["body"]["tel"]=m.tel;
+    this.json["body"]["pic"]=m.pic;
+
+    // 透過 JSON.parse() 解析 JSON 字串
+    let user = JSON.stringify(this.json);
+    var newstr = user
+
+    console.log("newstr"+newstr);
+
+    var objJsonArray =JSON.parse(newstr);  
+
+    return this.http.post('http://localhost:8080/merchant/save3',objJsonArray,this.headers)
+
+  }
+
+
 }
